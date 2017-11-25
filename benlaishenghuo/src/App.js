f7da9243ac2668833c3dfcbfd3fbdb80e20a892a
sunshinehahaha <1536821769@qq.com>
2017-11-25 13:10:24 +0800

    new file

--- benlaishenghuo/src/App.js
+++ benlaishenghuo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './sass/test.css';
 
 class App extends Component {
   render() {
@@ -13,6 +14,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div id="navbar">
+          <ul>
+            <li><a href="#">test</a></li>
+          </ul>
+        </div>
+
       </div>
     );
   }
