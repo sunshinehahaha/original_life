26e7d5d6459188caa2627787600a9ae21d266022
sunshinehahaha <1536821769@qq.com>
2017-11-27 13:40:39 +0800

    footer

--- benlaishenghuo/src/components/others/footer.js
+++ benlaishenghuo/src/components/others/footer.js
@@ -12,10 +12,36 @@ export default class Footer extends Component{
 		return (
 		    <div className="footer">
 			    <ul>
-			       <li><NavLink activeClassName="haha" to="/home">Home</NavLink></li>
-			       <li><NavLink activeClassName="haha" to="/classify">classify</NavLink></li>
-			       <li><NavLink activeClassName="haha" to="/cart">cart</NavLink></li>
-			       <li><NavLink activeClassName="haha" to="/my">my</NavLink></li>
+			        <li>
+			       		<NavLink activeClassName="haha" to="/home">
+			       			<i class="iconfont">&#xe663;</i>
+			       			<span>首页</span>
+			       		</NavLink>
+			       	</li>
+			        <li>
+			        	<NavLink activeClassName="haha" to="/classify">
+			        		<i class="iconfont">&#xe6b0;</i>
+			        		<span>分类</span>
+			        	</NavLink>
+			        </li>
+			        <li>
+			        	<NavLink activeClassName="haha" to="/#">
+			        		<i class="iconfont">&#xe61c;</i>
+			        		<span>充值</span>
+			        	</NavLink>
+			        </li>
+			        <li>
+			        	<NavLink activeClassName="haha" to="/cart">
+			        		<i class="iconfont">&#xe501;</i>
+			        		<span>购物车</span>
+			        	</NavLink>
+			        </li>
+			        <li>
+			        	<NavLink activeClassName="haha" to="/my">
+			        		<i class="iconfont">&#xe502;</i>
+			        		<span>我的本来</span>
+			        	</NavLink>
+			        </li>
 			    </ul>
 		    </div>
 		)
