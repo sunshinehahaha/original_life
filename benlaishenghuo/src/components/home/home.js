7a928fe59543d8bbb491b1ad5660953e1e6b5382
huangLByan <1203481241@qq.com>
2017-11-27 17:59:56 +0800

    com

--- benlaishenghuo/src/components/home/home.js
+++ benlaishenghuo/src/components/home/home.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import './home.css';
-<<<<<<< HEAD
+
 import {
   BrowserRouter as Router,
   Route,
@@ -8,26 +8,18 @@ import {
   NavLink,
   Redirect,
 } from 'react-router-dom';
-=======
+
 import Header from '../others/header.js';
 
 
->>>>>>> 18e0c8e5f87acaf64b6427677e27430281aca258
+
 class Home extends Component{
 	render (){
 		return (
 			<div id = "home">
 				<header className = "home_header">
 					<Header/>
-					<div className = "home_header_ipt">
-<<<<<<< HEAD
-						<input type = "text" placeholder = "优级果148元10斤"/>
-						<i className ="iconfont home_find">&#xe60b;</i>
-						<i className ="iconfont home_can">&#xe713;</i>
-=======
-						首页
->>>>>>> 18e0c8e5f87acaf64b6427677e27430281aca258
-					</div>
+					
 					<div className = "home_header_login">
 						<i className ="iconfont">&#xe502;</i>
 					</div>
