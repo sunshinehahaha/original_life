709cd07902ec924725ad20fdc29f27876160f0a1
huangLByan <1203481241@qq.com>
2017-11-28 11:18:56 +0800

    classify

--- benlaishenghuo/src/components/classify/classify.js
+++ benlaishenghuo/src/components/classify/classify.js
@@ -3,6 +3,13 @@ import Header from '../others/header.js';
 import {connect} from 'react-redux';
 import './sass/classify.css';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+  Redirect,
+} from 'react-router-dom';
 
 class ClassifyUI extends Component{
 	constructor(){
@@ -21,22 +28,22 @@ class ClassifyUI extends Component{
 	        	<Header/>
 	            <div className="left">
 	          		<ul>
-	          			<li><a href="#">镇店之宝</a></li>
-	          			<li><a href="#">水果</a></li>
-	          			<li><a href="#">蔬菜</a></li>
-	          			<li><a href="#">肉禽蛋品</a></li>
-	          			<li><a href="#">水产海鲜</a></li>
-	          			<li><a href="#">熟食面点</a></li>
-	          			<li><a href="#">粮油副食</a></li>
-	          			<li><a href="#">烘焙乳品</a></li>
-	          			<li><a href="#">休闲食品</a></li>
-	          			<li><a href="#">酒水茶饮</a></li>
-	          			<li><a href="#">厨房用品</a></li>
-	          			<li><a href="#">礼品礼盒</a></li>
-	          			<li><a href="#">休闲食品</a></li>
-	          			<li><a href="#">酒水茶饮</a></li>
-	          			<li><a href="#">厨房用品</a></li>
-	          			<li><a href="#">礼品礼盒</a></li>
+	          			<li><NavLink to = "/Storebaby">镇店之宝</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">水果</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">蔬菜</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">肉禽蛋品</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">水产海鲜</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">熟食面点</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">粮油副食</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">烘焙乳品</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">休闲食品</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">酒水茶饮</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">厨房用品</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">礼品礼盒</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">休闲食品</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">酒水茶饮</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">厨房用品</NavLink></li>
+	          			<li><NavLink to = "/Storebaby">礼品礼盒</NavLink></li>
 	          		</ul>
 	            </div>
 				<div className="right">
