98c0cab2e779c8ea7cd460397b05170f34e2c884
huangLByan <1203481241@qq.com>
2017-11-29 15:37:00 +0800

    cartall

--- benlaishenghuo/src/components/cart/cart.js
+++ benlaishenghuo/src/components/cart/cart.js
@@ -23,22 +23,23 @@ export default class Cart extends Component{
 				<main className = "cart_main">
 					<ul>
 						<li className = "cart_choose"><input type = "checkbox" className = "cart_main_ipt"/></li>
-						<li className = "cart_pic">物体图片</li>
+						<li className = "cart_pic">购物图片</li>
 						<li className = "cart_name">
 							<p className = "cart_name_name">物品名称</p>
 							<div className = "cart_name_mon">
 								<p className = "cart_name_onecost">单价</p>
-								<span className = "cart_cal">
-									<button>-</button>
-									<span>num</span>
-									<button>+</button>
-								</span>
+								<div className = "cart_cal">
+									<div>-</div>
+									<div>1</div>
+									<div>+</div>
+								</div>
 							</div>
 						</li>
 					</ul>
 				</main>
 				<footer className = "cart_footer">
-					<input type = "checkbox" />全选
+					<input type = "checkbox" />
+					<p className = "cart_chooseall">全选</p>
 					<p className = "cart_allcost">合计：<span></span></p>
 					<p className = "cart_pay">去结算(免运费)</p>
 				</footer>
