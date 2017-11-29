d12d192e7ed22b0e915d7e8d02ef8dab61b851f5
sunshinehahaha <1536821769@qq.com>
2017-11-29 18:18:11 +0800

    fruit

--- benlaishenghuo/src/components/classify/fruit.jsx
+++ benlaishenghuo/src/components/classify/fruit.jsx
@@ -11,12 +11,24 @@ import './sass/fruit.css';
 //   Switch
 // } from 'react-router-dom';
 
-class FruitUI extends Component{
+export default class Fruit extends Component{
 	constructor(){
 		super();
+		this.getFruitData = this.getFruitData.bind(this);
+		this.state = {
+			fruitList:[]
+		}
 	}
 	componentDidMount(){
-		this.props.getFruitData();
+		this.getFruitData();
+	}
+	getFruitData(){
+		var that = this;
+		axios.post('/api/search')
+			.then((res)=>{
+				console.log(res);
+				that.state.fruitList = res.data.data;
+			})
 	}
 	render(){
 		return(
@@ -25,78 +37,20 @@ class FruitUI extends Component{
 					<h3>热门推荐</h3>
 					<div className = "fruit_hot">
 						<ul className = "fruit_hot_ul">
-							<li>推荐水果</li>
-							<li>推荐水果</li>
-							<li>推荐水果</li>
-							<li>推荐水果</li>
-							<li>推荐水果</li>
-							<li>推荐水果</li>
-							<li>推荐水果</li>
+						{
+							this.state.fruitList.map((item,index)=>{
+								return (
+									<li key={item.sysNo}>
+										<img src={item.imgUrl}/>
+										<span>{item.name}</span>
+									</li>
+								)
+							})
+						}
 						</ul>
 					</div>
-					<h3>礼盒/组合</h3>
-					<div className = "fruit_com">
-						<ul className = "fruit_com_ul">
-							<li>组合水果</li>
-							<li>组合水果</li>
-							<li>组合水果</li>
-						</ul>
-					</div>
-					<h3>精选水果</h3>
-					<div className = "fruit_cho">
-						<ul className = "fruit_cho_ul">
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-							<li>精选水果</li>
-						</ul>
-					</div>
-					<div className = "fruit_null"></div>
+					
 				</div>
 			)
 	}
 }
-
-const mapStateToProps = (state)=>{
-	return {
-		fruitList:state.fruitList
-	}
-}
-
-const mapDispatchToProps = (dispacth)=>{
-	return {
-		getFruitData:function(){	
-			console.log("Fruit");
-			axios.post('/api/search')
-			.then((res)=>{
-				console.log(res);
-				// dispacth({
-				// 	type:"FRUIT_DATA",
-				// 	payload:res
-				// })
-			})
-		}
-		
-	}
-}
-
-const Fruit = connect(mapStateToProps,mapDispatchToProps)(FruitUI);
-
-export default Fruit;
