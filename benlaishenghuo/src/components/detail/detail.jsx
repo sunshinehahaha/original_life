615a748b8502f62b229146b02d8b8586c22a8be5
huangLByan <1203481241@qq.com>
2017-11-29 20:01:35 +0800

    huang

--- benlaishenghuo/src/components/detail/detail.jsx
+++ benlaishenghuo/src/components/detail/detail.jsx
@@ -17,8 +17,8 @@ class Detail extends Component{
 						<p className = "detail_header_pos">大连</p>
 						<h3>商品</h3>
 					</header>
-					<main className = "deatil_main">
-						<div className = "detail_main_pic">这是一个大商品详情的图片</div>
+					<main className = "detail_main">
+						<div className = "detail_main_pic">这是详情图片</div>
 						<div className = "detail_main_list">
 							<div className = "detail_main_listname">这是商品的名字</div>
 							<div className = "detail_main_cost">这是单价</div>
