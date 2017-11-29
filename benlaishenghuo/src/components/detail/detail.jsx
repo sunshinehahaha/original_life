97efc2eb77966bc046852683445f4e99c81f95ea
huangLByan <1203481241@qq.com>
2017-11-29 19:54:36 +0800

    detail

--- benlaishenghuo/src/components/detail/detail.jsx
+++ benlaishenghuo/src/components/detail/detail.jsx
@@ -9,9 +9,9 @@ import{
 class Detail extends Component{
 	render(){
 		return(
-				<div id = "deatil">
+				<div id = "detail">
 					<header className = "detail_header">
-						<NavLink to = "/home">
+						<NavLink to = "/classify">
 							<i className ="iconfont detail_header_icon">&#xe607;</i>
 						</NavLink>
 						<p className = "detail_header_pos">大连</p>
@@ -21,10 +21,15 @@ class Detail extends Component{
 						<div className = "detail_main_pic">这是一个大商品详情的图片</div>
 						<div className = "detail_main_list">
 							<div className = "detail_main_listname">这是商品的名字</div>
-							<div className = "detail_main_list">这是商品描述</div>
 							<div className = "detail_main_cost">这是单价</div>
 						</div>
 					</main>
+					<footer className = "detail_footer">
+						<div className = "detail_footer_add">
+							<i className ="iconfont detail_footer_icon">&#xe501;</i>
+						</div>
+						<div className = "detail_footer_info">无法送达</div>
+					</footer>
 				</div>
 			)
 	}
